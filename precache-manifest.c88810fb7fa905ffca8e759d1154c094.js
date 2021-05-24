0e3b589b097dbbca7e9d69db4c266a0be33a7ac7
Rishabh Jain <jain.rishabhche@gmail.com>
2021-05-24 17:53:21 +0530

    Updates

---
+++ precache-manifest.c88810fb7fa905ffca8e759d1154c094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8f58396c6b06654e379f2fc7da8adb",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2bea5fad453c764094d1",
+    "url": "/crwn-clothing/static/css/main.27574900.chunk.css"
+  },
+  {
+    "revision": "c71704fad8d08bbffa63",
+    "url": "/crwn-clothing/static/js/2.165417c2.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-clothing/static/js/2.165417c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bea5fad453c764094d1",
+    "url": "/crwn-clothing/static/js/main.3c07c1be.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  }
+]);
